e1ac04a4963512277cd0481dfdca0a029c11a767
Billy Kinyanjui <billy.kinyanjui@student.moringaschool.com>
2022-08-22 20:57:19 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
-}
+function addingEventListener(childAlert) {
+    const input = document.getElementById('button');
+    function clickAlert(e) {  
+      alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
+  } 
+ 
+
